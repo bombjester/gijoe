9ca69870ea1990a3a043114118fed4ec49412144
Eric Lee <e.keith.lee@gmail.com>
2021-01-18 14:13:02 -0800

    atlas migration

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -4,7 +4,8 @@ var mongoose = require('mongoose');
 var fs = require('fs');
 // connect to the database
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://admin:password@ds141175.mlab.com:41175/gijoe');
+// mongoose.connect('mongodb://admin:password@ds141175.mlab.com:41175/gijoe');
+mongoose.connect('mongodb://admin:password@gijoe-shard-00-00.po6jx.mongodb.net:27017,gijoe-shard-00-01.po6jx.mongodb.net:27017,gijoe-shard-00-02.po6jx.mongodb.net:27017/gijoe?ssl=true&replicaSet=atlas-ri5at9-shard-0&authSource=admin&retryWrites=true&w=majority');
 // specify the path to all of the models
 var models_path = __dirname + '/../models'
 // read all of the files in the models_path and for each one check if it is a javascript file before requiring it
